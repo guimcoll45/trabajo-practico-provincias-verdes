142ef39c4a5aeb03e328737557603fb3385e4708
guimcoll45 <guimcoll45@gmail.com>
2026-06-16 10:25:39 -0300

    Create Main class with entry point for application

---
+++ src/com/provinciasverdes/clase principal/Main.java
@@ -0,0 +1,11 @@
+package com.provinciasverdes;
+
+import com.provinciasverdes.presentacion.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        // Punto de entrada del programa
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar();
+    }
+}
