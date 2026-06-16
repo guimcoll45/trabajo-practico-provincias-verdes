98e150fe2f3f6ffb3b2bc1a3a532bc94dfe85e62
guimcoll45 <guimcoll45@gmail.com>
2026-06-16 10:34:20 -0300

    Add Main class to start the application

---
+++ src/com/provinciasverdes/presentacion/Main.java
@@ -0,0 +1,8 @@
+package com.provinciasverdes.presentacion;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar();
+    }
+}
